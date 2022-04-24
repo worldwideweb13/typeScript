a0d3959468ab2162ae564c32b5e5e42f3ab5a850
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-04-24 23:55:44 +0900

    デコレーターでのvalidaition実装

--- TS-metaData/src/app.ts
+++ TS-metaData/src/app.ts
@@ -1,7 +1,9 @@
-// 一般的にデコレーターはクラスに追加するもの
-// デコレーターは最終的にはただの関数
-// デコレーターはクラス宣言の直前に@関数名で宣言することで、対象クラスに@"関数名"の関数を適用する
-// デコレーターが呼び出させるのはクラスをnewする時ではない。JSがクラスの定義を見つけた時に実行される。
+/* 一般的にデコレーターはクラスに追加するもの
+   デコレーターは最終的にはただの関数
+   デコレーターはクラス宣言の直前に@関数名で宣言することで、対象クラスに@"関数名"の関数を適用する
+   デコレーターが呼び出させるのはクラスをnewする時ではない。JSがクラスの定義を見つけた時に実行される。
+  デコレーターはクラスの初期設定に関する何かを行う。   
+*/ 
 
 // デコレーターファクトリ...デコレーターを何かに割り当てる時にカスタマイズできる機能
 function Logger(logString: string) {
@@ -17,14 +19,32 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log('TEMPLATE ファクトリ');
-  return function(constructor: any) {
-    console.log('テンプレートを表示');
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+  /* デコレータ関数の実行部分はクラス宣言時に実行される
+     -T extend {new}...{}は引数にクラスを持つことを表す. 
+      newは予約語で"new"というキーワードを使って、オブジェクトを作ることができる関数=constructor関数 という意味を表す
+      newは任意の数の引数を受け取るためレストパラメーター(...arg)をany型の配列(any:[])で設定
+     -new(...args: any[]): {name: string}...new関数はオブジェクトを返す{}.
+      返されるオブジェクトにはnameというプロパティが必ず入っているため、 {name: string}
+  */ 
+  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+    /* 呼び出し元のconstructor(originalConstructor)を継承した新しいクラスに置き換える
+       置き換えたクラスは、クラスのインスタンス化されたタイミングで実行される
+       デコレーターファクトリーでクラスを書き換えることで、インスタンス作成時に実行されるデコレータ関数を作成できた
+    */
+    return class extends originalConstructor {
+      // 新しいクラス(無名クラス)にconstructorを設定する
+      // _...引数として設定する必要はあるが、今は利用することがない時、"_"と記述する
+      constructor(..._: any[]){
+        super(); // originalConstructor(呼び出し元のconstructor関数)を実行（継承）
+        console.log('テンプレートを表示');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }    
+      }
     }
+
   };
 }
 
@@ -42,28 +62,56 @@ class Person {
   }
 }
 
-const pers = new Person();
+// const pers = new Person();
 
-console.log(pers);
+// console.log(pers);
 
-// デコレーター関数
+
+//プロパティにデコレーターを使う(プロパティデコレーター)場合
 // 引数は２つ設定
 //  1(target)はインスタンスのプロパティにデコレーターを設定した場合、targetにはクラスのプロトタイプが渡される
 //  1(target)はスタティックのプロパティにデコレーターを設定した場合、targetにはコンストラクタ関数が渡される
+// プロトタイプ...クラスのプロパティを抜いたクラス内の、constructor,関数一覧を示す構造体
 //  2(propertyName)の名前
 function Log(target: any, propertyName: string | Symbol){
   console.log("property デコレータ");
   console.log(target,propertyName);
 }
 
+/* アクセサーデコレーター
+PropertyDescriptor...オブジェクトのプロパティのメタ属性でデータアクセスに関する取り決めを保持している。 
+ 例えば,writable(書き換え可否), configurable(ディスクリプタの変更可否), enumerable(プロパティ列挙に現れるか否か)
+ 詳細は https://00m.in/HsK5k 参照
+*/
+function Log2(target: any, name: string, descripter: PropertyDescriptor){
+  console.log("Accssor デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descripter);
+}
 
-//プロパティにデコレーターを使う(プロパティデコレーター)場合
+// メソッドデコレーター
+function Log3(target: any, name: string | symbol, descripter: PropertyDescriptor) {
+  console.log("Method デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descripter);
+}
+
+// パラメータデコレータ
+function Log4(target: any, name: string | Symbol, position: number){
+  console.log("Parameter デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
 
 class Product {
   @Log
   title: String;
   private _price: number;
 
+  @Log2
   // priceのセッター
   set price(val: number){
     if(val > 0){
@@ -73,14 +121,131 @@ class Product {
     }
   }
 
-
   constructor(t: string, p: number){
     this.title = t;
     this._price = p;
   }
 
-  getPriceWithTax(tax: number){
+  @Log3
+  getPriceWithTax(@Log4 tax: number){
     return this._price * (1 + tax);
   }
 
 }
+
+// デコレーターはインスタンス化した時に呼び出される訳ではない。
+const p1 = new Product('Book', 100);
+const p2 = new Product('Book2', 200);
+
+function Autobind(_: any, _2: string, descripter: PropertyDescriptor){
+  const originalMethod = descripter.value;
+  const adjDescrupter: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescrupter;
+}
+
+
+class Printer {
+  message = 'クリックしました!';
+
+  @Autobind
+  showMessage(){
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener('click', p.showMessage);
+
+// ---
+
+interface ValidatorConfig {
+  // key : {key:val} 
+  [prop: string]: {
+    [validatable: string]: string[] // ['required', 'positive']
+  }
+}
+
+const registerValidators: ValidatorConfig  = {};
+
+// Courseインスタンスのプロパティtitleのプロパティデコレーター
+function Required(target: any, propName: string) {
+  // target (プロトタイプ)のconstructor関数名を参照. それをregisterValidatorsのkeyに設定する
+  registerValidators[target.constructor.name] = {
+    ...registerValidators[target.constructor.name],
+    [propName]: ['required'],
+  }
+}
+
+// Courseインスタンスのプロパティpriceのプロパティデコレーター
+function PositiveNumber(target: any, propName: string) {
+  // target (プロトタイプ)のconstructor関数名を参照. それをregisterValidatorsのkeyに設定する
+  registerValidators[target.constructor.name] = {
+    ...registerValidators[target.constructor.name],
+    [propName]: ['positive'],
+  }
+}
+
+
+let isValid = true;
+function validate(obj: any){
+  const objValidatorConfig = registerValidators[obj.constructor.name];
+  console.log(objValidatorConfig);
+  // 何もプロパティに値が設定されていないときはtrueで返す
+  if(!objValidatorConfig) {
+    return true;
+  }
+  for(const prop in objValidatorConfig){
+    console.log(prop);
+    for(const validator of objValidatorConfig[prop]){
+      switch(validator) {
+        case 'required':
+          isValid =  isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid =  isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+  constructor(t: string, p : number){
+    this.title = t;
+    this.price = p;
+  }
+}
+
+// validationの実装
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  // +...number型にキャスト
+  const price = +priceEl.value;
+// デコレーター関数
+  const createCourse = new Course(title, price);
+  if(!validate(createCourse)){
+    alert('正しく入力して下さい！');
+    return;
+  }
+  console.log(createCourse);
+});

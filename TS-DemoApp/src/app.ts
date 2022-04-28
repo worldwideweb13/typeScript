44eee6dc4383db7fde87ea6cc442bd3ed25ccaaf
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-04-28 19:27:31 +0900

    プロジェクトのstatus管理クラスの追加

--- TS-DemoApp/src/app.ts
+++ TS-DemoApp/src/app.ts
@@ -1,3 +1,77 @@
+// プロジェクト（ユーザーが登録するプロジェクト）のType
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public manday: number,
+    public status: ProjectStatus
+  ) {}
+}
+
+// project State Management...タスクの状態管理クラス
+type listnerer = (items: Project[]) => void;
+
+class ProjectState {
+  // listnerers...関数の配列
+  private listnerers: listnerer[] = [];
+  //  タスク一覧を管理するプロパティ
+  private projects: Project[] = [];
+  // アプリケーション全体で必ず一つのインスタンスしか存在しない
+  private static instance: ProjectState;
+
+  // private constructor...シングルトン。インスタンスが一つしかないことを保証するデザインパターン
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  // 関数を引数に受け取ってlistnerersに格納する
+  addlistnerers(listnerersFn: listnerer) {
+    this.listnerers.push(listnerersFn);
+  }
+
+  // プロジェクトを新たに追加する関数
+  // この関数はタスクの登録ボタンが押された直後に実行される
+  addProject(title: string, description: string, manday: number) {
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      manday,
+      // ProjectStatusのデフォルト値はActiveに設定
+      ProjectStatus.Active
+    );
+    this.projects.push(newProject);
+    /* 
+         for(val of objects){}...反復可能なobjectsから要素を一つずつ取り出して{}の処理を実行する
+         今回は関数を配列形式で保持する listnerers から関数を取り出して実行する
+         実行する関数には引数にプロジェクト一覧の配列(projects)を渡す         
+    */
+    for (const listnerersFn of this.listnerers) {
+      /* 
+        const listnerersFn = 関数 なので listnerersFn(引数) で関数実行
+        projectsはどこからでも書き換え可能なので、引数として無闇に利用するとバグの温床になる
+        そのため、slice()メソッドでコピーした引数を利用するようにする
+      */
+      listnerersFn(this.projects.slice());
+    }
+  }
+}
+
+// シングルトンで状態管理クラス（ProjectState）のインスタンス発行
+const projectState = ProjectState.getInstance();
+
 // validation
 interface Validatable {
   value: string | number;
@@ -72,6 +146,79 @@ function autobind(
   return adjDescripter;
 }
 
+// projectList Class
+class projectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  assignedProjects: Project[];
+
+  // constructor引数にリテラル or ユニオン型の'type'を設定
+  constructor(private type: "active" | "finished") {
+    // コンテンツを表示するためにアクセスしなければいけない要素へのアクセス修飾子
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    //  初期化
+    this.assignedProjects = [];
+
+    /* importNode(arg1,bool)...arg1に取得するDOM要素、bool = deepCloneフラグ。arg1の下の階層の要素も取得する場合はtrue,
+             templateElement.content...templateエレメントの内側の要素を取得したいときは'.content'
+      */
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    // firstElementChild...直下の子要素(importedNode直下の子要素<section id='projects'> をHTMLElementとして取得)
+    this.element = importedNode.firstElementChild as HTMLElement;
+    // DOM要素に'user-input'タグを追加（CSSを適用）
+    this.element.id = `${this.type}-projects`;
+
+    /*  新しいプロジェクトが追加された時にaddListner関数実行。
+        addListner関数は、ProjectState.listnerers に関数を配列形式で保持します
+        addListner関数は引数に関数を取るため、引数に関数式を入れます...addListner(() => {})
+        配列に保持する関数はrenderProjects()です。この関数はユーザーがプロジェクトの'登録'ボタンを押下した時に実行されます
+         →submitHandler() → addProject() → for (const listnerersFn of this.listnerers)...の順で実行されます
+    */
+    projectState.addlistnerers((projects: Project[]) => {
+      this.assignedProjects = projects;
+      // プロジェクト一覧を active or finished-projects-listタグ直下にliタグで挿入する
+      this.renderProjects();
+    });
+    // index.html にrenderProjects() で描画したHTMLコンテンツを挿入
+    this.attach();
+    this.renderContent();
+  }
+
+  // プロジェクト一覧をhtml要素に追加する関数
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = prjItem.title;
+      listEl.appendChild(listItem);
+    }
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    // ul要素のid名にactive-projects-list or finished-projects-list を追加
+    this.element.querySelector("ul")!.id = listId;
+    // h2要素のテキストに実行中プロジェクト / 完了プロジェクト を追加
+    this.element.querySelector("h2")!.textContent =
+      this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+  }
+  // index.html にrenderProjects() で描画したHTMLコンテンツを挿入する関数
+  // beforeend...element 内部の、最後の子要素の後に挿入
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+// projectInput Class
 class projectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -158,6 +305,7 @@ class projectInput {
 
   // デコレーターの利用
   @autobind
+  // 登録ボタンが押された時に実行される関数
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
@@ -166,7 +314,8 @@ class projectInput {
     // JSではタプル型は配列になるため、Array.isArray(array)関数でarrayが配列化をチェック
     if (Array.isArray(userInput)) {
       const [title, desc, manday] = userInput;
-      console.log(title, desc, manday);
+      // 状態管理クラス（ProjectState）のインスタンス（ProjectState）にタスクを追加
+      projectState.addProject(title, desc, manday);
       this.clearInputs();
     }
   }
@@ -188,3 +337,5 @@ class projectInput {
 
 // インスタンスの生成
 const prjInput = new projectInput();
+const activePrjList = new projectList("active");
+const finishedPrhList = new projectList("finished");

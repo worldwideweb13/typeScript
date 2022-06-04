98eb40c280d02d76140827e4d298ac26bae69416
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-04 21:15:27 +0900

    vue.js環境構築

---
+++ ts-vue-grammer/vue-sample/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello world" />
+  <label for="" class="label">Vue Learning</label>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.label {
+  font-size: 12px
+}
+</style>

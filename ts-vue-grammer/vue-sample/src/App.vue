0459733bfcab1a63b3e5e652c0385704e1a87a85
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-04 21:54:29 +0900

    変数の渡し方,ディレクティブ

--- ts-vue-grammer/vue-sample/src/App.vue
+++ ts-vue-grammer/vue-sample/src/App.vue
@@ -2,12 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import PaymentVue from './components/Payment.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello world" />
-  <label for="" class="label">Vue Learning</label>
+  <PaymentVue />
 </template>
 
 <style>
---
+++ ts-vue-grammer/vue-sample/src/components/Payment.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const itemName1 = "Desk";
+const itemName2 = "Bike";
+const price1 = 400;
+const price2 = 380000;
+const url1 = "https://00m.in/MfVce"
+
+const buy = (itemName:string) => {
+    alert("Are you sure to buy " + itemName + "?")
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>最近の支出</h1>
+    <div class="payment">
+      <label>{{ itemName1 }}</label>
+      <label>${{ price1 }}</label>
+      <a v-bind:href="url1">bought at...</a>
+      <button v-on:click="buy(itemName1)">buy</button>
+    </div>
+    <div class="payment">
+      <label>{{ itemName2 }}</label>
+      <label>${{ price2 }}</label>
+      <button v-on:click="buy(itemName2)">buy</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.payment {
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  height: 80px;
+  background-color: #f0f8ff;
+  align-items: center;
+  margin-bottom: 8px;
+}
+
+label {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>

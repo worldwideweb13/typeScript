005d6a7643e6b4fa40dc53ea055f05a0e36fe0fe
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-08 19:55:01 +0900

    静的なリスト

---
+++ ts-vue-grammer/vue-sample/src/components/Tweet.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+const tweets = [
+  {
+    id: "0",
+    description: "Hello, Mike,",
+  },
+  {
+    id: "1",
+    description: "Hello, Ken,",
+  },
+];
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input />
+      <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.save-button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
+}
+
+.save-button:hover {
+  background-color: #1baaaa;
+}
+.tweet-list {
+  list-style: none;
+}
+
+.delete-button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #c99a68;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
+}
+
+.delete-button:hover {
+  background-color: #9d6324;
+}
+
+input {
+  margin-bottom: 16px;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+</style>

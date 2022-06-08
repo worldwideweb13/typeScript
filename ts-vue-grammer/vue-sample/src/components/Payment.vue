47968c27a0e13c7c43e1da08ff4705a043c1777b
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-08 16:46:52 +0900

    RefとReactive

--- ts-vue-grammer/vue-sample/src/components/Payment.vue
+++ ts-vue-grammer/vue-sample/src/components/Payment.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-const itemName1 = "Desk";
+import { ref } from "vue";
+
+const itemName1 = ref<string>("Desk");
 const itemName2 = "Bike";
 const price1 = 400;
 const price2 = 380000;
-const url1 = "https://00m.in/MfVce"
+const url1 = "https://00m.in/MfVce";
 
-const buy = (itemName:string) => {
-    alert("Are you sure to buy " + itemName + "?")
-}
+const buy = (itemName: string) => {
+  alert("Are you sure to buy " + itemName + "?");
+};
 
+
+const input = (event: any) => {
+  console.log("event", event.target.value);
+  // objectそのものが定数(const)であっても中のプロパティに関しては変更可能
+  itemName1.value = event.target.value;
+};
 </script>
 
 <template>
   <div class="container">
     <h1>最近の支出</h1>
+    <input v-on:input="input" />
     <div class="payment">
       <label>{{ itemName1 }}</label>
       <label>${{ price1 }}</label>

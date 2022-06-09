6a38f6d2d37e3d95c61f5bbeb0c062dfdb16442e
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-09 17:21:15 +0900

    refactor:全体の構成を作る

---
+++ ts-vue-grammer/PostApp/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Persons from './components/Persons.vue';
+</script>
+
+<template>
+  <Persons />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

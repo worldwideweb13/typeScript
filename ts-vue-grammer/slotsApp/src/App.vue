dc94b905ebacf888107b30d7506d6ba1f48f5552
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-10 21:41:05 +0900

    動的なコンポーネント

--- ts-vue-grammer/slotsApp/src/App.vue
+++ ts-vue-grammer/slotsApp/src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import CardListVue from "./components/CardList.vue";
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+import CardList from "./components/CardList.vue";
+import SecondCardList from "./components/SecondCardList.vue";
+
+const isActiveTab = ref("1");
+const updateTab = (isFirst: string) => {
+  isActiveTab.value = isFirst;
+};
+
+const currentComponent = computed(() => {
+  switch (isActiveTab.value) {
+    case '1':
+      return CardList;
+    case '2':
+      return SecondCardList;
+  }
+});
 </script>
 
 <template>
-  <CardListVue></CardListVue>
+  <div class="tab-changer">
+    <button @click="updateTab('1')">Tab 1</button>
+    <button @click="updateTab('2')">Tab 2</button>
+  </div>
+  <div class="tab-contents">
+    <component :is="currentComponent"></component>
+    <!-- <CardList v-if="isFirstTab"></CardList>
+    <SecondCardList v-if="!isFirstTab"></SecondCardList> -->
+  </div>
 </template>
 
 <style>

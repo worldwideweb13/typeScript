8e89e8928d166bb4c5c90aa88425978c3bb039fd
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-16 18:23:13 +0900

    Provide / inject

---
+++ ts-vue-grammer/provideInjectApp/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TodoListVue from "./components/TodoList.vue";
+import { provide } from "vue";
+import { addTodo, todos } from "./useTodo";
+import TodoList from "./components/TodoList.vue";
+
+provide("todos", todos);
+provide("addTodo", addTodo);
+</script>
+
+<template>
+  <TodoList />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>

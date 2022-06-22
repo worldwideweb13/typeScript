f3641358e39792850a4b4842cb269414d363205a
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-22 17:56:31 +0900

    Modalのアニメーション

---
+++ ts-vue-grammer/animationSample/src/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="backdrop" v-if="open"></div>
+  <transition name="modal">
+    <dialog open v-if="open" class="dialog">
+      <slot></slot>
+    </dialog>
+  </transition>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  open: boolean;
+};
+
+defineProps<Props>();
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.644);
+}
+
+.dialog {
+  position: fixed;
+  top: 30vh;
+  width: 30rem;
+  left: calc(50% - 15rem);
+  margin: 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 25);
+  border-radius: 12px;
+  padding: 1rem;
+  background-color: white;
+  z-index: 100;
+  border: none;
+}
+
+.modal-enter-active {
+  animation: modal 1s ease-out;
+}
+
+.modal-leave-active {
+  animation: modal 1s ease-out reverse;
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+}
+</style>

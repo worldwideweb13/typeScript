7e9e52ef37ff8a1be5878d52e559017ce85b1169
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-08-07 16:08:31 +0900

    publicPath (vue.config.js)

--- ts-vue-grammer/vue-grammer-memo/section6/cli-lesson-1/src/modules/TestComponent.vue
+++ ts-vue-grammer/vue-grammer-memo/section6/cli-lesson-1/src/modules/TestComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">TestComponentの本文です。{{ testData }}</div>
+  <div class="red-box">TestComponentの本文です。{{ testData }}</div>
 </template>
 
 <script>
@@ -12,4 +12,8 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+.red-box {
+  border: 1px red solid;
+}
+</style>

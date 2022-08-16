293efb92297f7f76a915c9eecd18bb310389397e
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-08-16 23:19:18 +0900

    動的パラメータ+props その1

---
+++ ts-vue-grammer/vue-grammer-memo/section6/vue-router/src/views/BookList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>本の一覧</h1>
+    <ul>
+      <li @click="showBookDetail(book.id)" v-for="book in books" :key="book.id">
+        {{ book.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+  data() {
+    return {
+      bookIndex: -1,
+      books: [
+        { id: 1, author: "夏目漱石", title: "我輩は猫である" },
+        { id: 2, author: "坂口安吾", title: "堕落論" },
+        { id: 3, author: "太宰治", title: "人間失格" },
+      ],
+    };
+  },
+  methods: {
+    showBookDetail(id) {
+      this.bookIndex = id - 1;
+      console.log(this.bookIndex);
+    },
+  },
+};
+</script>
+
+<style></style>

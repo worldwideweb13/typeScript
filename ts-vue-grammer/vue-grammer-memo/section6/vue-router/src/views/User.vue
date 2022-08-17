2eebed90a3366affa3b788f749e79dbb0d7d158f
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-08-18 01:39:57 +0900

    ネストされたルート

---
+++ ts-vue-grammer/vue-grammer-memo/section6/vue-router/src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    ユーザー情報<br />
+    <router-link to="profile">プロフィール</router-link> |
+    <router-link to="post">ユーザー記事</router-link> |
+    <div class="green-b">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.green-b {
+  border: 1px solid green;
+}
+</style>

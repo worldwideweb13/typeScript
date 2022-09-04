2ad335ba00a82b6319cd6facd99e472ea490bf8d
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-09-05 01:33:03 +0900

    App.vueからlocalStorageへ保存

--- ts-vue-grammer/vue-grammer-memo/section8/bookapp/src/pages/BookSearch.vue
+++ ts-vue-grammer/vue-grammer-memo/section8/bookapp/src/pages/BookSearch.vue
@@ -1,55 +1,53 @@
 <template>
   <div>
-    <v-container>
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            label="本のタイトルを検索"
-            v-model="keyword"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3">
-          <v-btn color="primary" @click="search(keyword)"> 検索する</v-btn>
-        </v-col>
-        <v-col cols="3">
-          <v-btn color="secondary" to="/">一覧に戻る</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-          v-for="(book, index) in searchResults"
-          :key="book.index"
-        >
-          <v-card class="mx-auto">
-            <v-row>
-              <v-col cols="4">
-                <v-img :src="book.image"></v-img>
-              </v-col>
-              <v-col cols="8">
-                <v-card-title>{{ book.title }}</v-card-title>
-                {{ book.description }}
-                <v-spacer></v-spacer>
-                <v-card-action>
-                  <v-btn
-                    class="mx-2"
-                    fab
-                    dark
-                    color="indigo"
-                    @click="addBookList(index)"
-                  >
-                    <v-icon dark> mdi-plus </v-icon>
-                  </v-btn>
-                </v-card-action>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+          label="本のタイトルを検索"
+          v-model="keyword"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <v-btn color="primary" @click="search(keyword)"> 検索する</v-btn>
+      </v-col>
+      <v-col cols="3">
+        <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+        v-for="(book, index) in searchResults"
+        :key="book.index"
+      >
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              {{ book.description }}
+              <v-spacer></v-spacer>
+              <v-card-action>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="indigo"
+                  @click="addBookList(index)"
+                >
+                  <v-icon dark> mdi-plus </v-icon>
+                </v-btn>
+              </v-card-action>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -63,6 +61,9 @@ export default {
     };
   },
   methods: {
+    addBookList(index) {
+      this.$emit("add-book-list", this.searchResults[index]);
+    },
     async search(keyword) {
       this.searchResults = [];
       const baseUrl = "https://www.googleapis.com/books/v1/volumes?";

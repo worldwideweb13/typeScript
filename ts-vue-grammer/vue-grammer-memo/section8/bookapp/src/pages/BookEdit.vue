82aa56642ebedf37446bb30ffc16215b2fd6ca4c
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-09-09 01:14:02 +0900

    BookEdit カレンダーなど

--- ts-vue-grammer/vue-grammer-memo/section8/bookapp/src/pages/BookEdit.vue
+++ ts-vue-grammer/vue-grammer-memo/section8/bookapp/src/pages/BookEdit.vue
@@ -1,6 +1,52 @@
 <template>
   <div>
-    BookEdit
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title> タイトル: {{ book.title }} </v-card-title>
+              読んだ日:
+              <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="date"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  @input="menu = false"
+                  locale="jp-ja"
+                  :day-format="(date) => new Date(date).getDate()"
+                ></v-date-picker>
+              </v-menu>
+              感想:
+              <v-textarea class="mx-2" v-model="book.memo">{{
+                book.memo
+              }}</v-textarea>
+            </v-col>
+            <v-card-actions>
+              <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+              <v-btn color="info" @click="editBookInfo">保存する</v-btn>
+            </v-card-actions>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+
     {{ book.title }}
   </div>
 </template>
@@ -14,13 +60,19 @@ export default {
   data() {
     return {
       book: "",
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      menu: false,
     };
   },
   beforeRouteEnter(to, from, next) {
     next((vm) => {
       //`vm`を通じてコンポーネントインスタンスにアクセス
-      vm.book = vm.books[vm.$route.params.id]
-      console.log(vm.book);
+      vm.$nextTick(() => {
+        vm.book = vm.books[vm.$route.params.id];
+        console.log(vm.book);
+      });
     });
   },
 };

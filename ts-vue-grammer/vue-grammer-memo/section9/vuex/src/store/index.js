8f7b01bdc2a0f88ed882dc88c86657d75b3e726e
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-09-14 23:58:55 +0900

    Vuex stateとmutations & mutations補足 payload

--- ts-vue-grammer/vue-grammer-memo/section9/vuex/src/store/index.js
+++ ts-vue-grammer/vue-grammer-memo/section9/vuex/src/store/index.js
@@ -1,17 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    increment(state) {
+      state.count++;
+    },    
+    // 第2引数はオブジェクト
+    addCount(state, payload) {
+      state.count += payload.value;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});

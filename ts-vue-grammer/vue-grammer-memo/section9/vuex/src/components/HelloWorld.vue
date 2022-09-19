ddbc521cfa80b8dcecd9bb3d19ea2f34d6120002
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-09-20 01:47:49 +0900

    mapヘルパー(mapActions)

--- ts-vue-grammer/vue-grammer-memo/section9/vuex/src/components/HelloWorld.vue
+++ ts-vue-grammer/vue-grammer-memo/section9/vuex/src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <button @click="increment">+</button>
+    <!-- <button @click="increment">+</button> -->
+    <button @click="incrementAction">+</button>
     <button @click="addCount">+10</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "HelloWorld",
   props: {
@@ -20,11 +22,17 @@ export default {
     //     value: 10,
     //   });
     // },
-    increment() {
-      this.$store.dispatch("incrementAction");
-    },
+    ...mapActions(["incrementAction", "addCountAction"]),
+    // increment() {
+    //   this.$store.dispatch("incrementAction");
+    // },
+    // addCount() {
+    //   this.$store.dispatch("addCountAction", {
+    //     value: 10,
+    //   });
+    // },
     addCount() {
-      this.$store.dispatch("addCountAction", {
+      this.addCountAction({
         value: 10,
       });
     },
